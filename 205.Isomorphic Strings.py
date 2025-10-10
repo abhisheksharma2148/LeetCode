d0faa3611eb997d35873f1054ae8e455ad7e66a1
Abhishek Sharma <71930900+abhisheksharma2148@users.noreply.github.com>
2025-10-10 10:54:40 +0530

    Create 205.Isomorphic Strings.py

---
+++ 205.Isomorphic Strings.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s2t, t2s = {}, {}
+        for a,b in zip(s,t):
+            if (a in s2t and s2t[a]!=b) or (b in t2s and t2s[b]!=a):
+                return False
+            s2t[a] = b
+            t2s[b] = a
+        return True
+        
